2a3adb7124dadd7676aedd108e5003f366d00341
dagm <dadgmg0987@gmial.com>
2024-06-29 16:06:57 -0700

    worked menu bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import './css/Header.css';
 import './css/Responsive css/header_res.css';
 import { useNavigate } from "react-router-dom";
+import Menu from "./Menu";
 
 function Header({props}) {
-
+    const [menu, setMenu] = useState();
     const navigate = useNavigate();
 
     const gotohome = (e) => {
@@ -45,12 +46,21 @@ function Header({props}) {
     // }  if (props = 'contact') {
     //     contact_active = 'active';
     // }
+    const closepop = () => {
+        setMenu(null);
+    }
 
     return(
         <>
+            {menu}
             <div className="header">
                 <div className="logo">
                     <h2>Dagos</h2>
+                </div>
+                <div className="menu-icon">
+                    <a onClick={() => {setMenu(<Menu closepopup={closepop}/>)}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+                    </a>
                 </div>
                 <div className="menu">
                     <a href="/" onClick={gotohome}>Home</a>
